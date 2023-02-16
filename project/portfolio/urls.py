782839f61c948e50d8a0401d043f754864bbbea3
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-16 19:12:44 +0100

    Changing the structure of the app

--- project/portfolio/urls.py
+++ project/portfolio/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import home
+from .views import home,contact,about
 
 urlpatterns = [
     path('', home, name='home'),
+    path('about', about, name='about'),
+    path('contact', contact, name='contact'),
 ]
