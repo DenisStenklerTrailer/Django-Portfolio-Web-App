dd38fa56f3bfa590ba14add9b63864f326dbc589
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-18 06:50:17 +0100

    started programming sign up page (HTML)

--- project/authapp/urls.py
+++ project/authapp/urls.py
@@ -4,6 +4,6 @@ from .views import signup,handleLogin,handleLogout
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
-    path('login', handleLogin, name='handleLogin'),
+    path('login/', handleLogin, name='handleLogin'),
     path('logout/', handleLogout, name='handleLogout'),
 ]
