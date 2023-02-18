cdd8495c4ecb692d3cfe462e1d3c56c890d9753d
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-18 06:29:27 +0100

    prepared all functions al urls for login implemention

---
+++ project/authapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from .views import signup,handleLogin,handleLogout
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('login', handleLogin, name='handleLogin'),
+    path('logout/', handleLogout, name='handleLogout'),
+]
