3b86691404aa80fb6fd3db363bf180879ecf5d1a
DenisStenklerTrailer <77147548+DenisStenklerTrailer@users.noreply.github.com>
2023-02-19 07:32:48 +0100

    Implementing backend functions for login/signup

--- project/authapp/views.py
+++ project/authapp/views.py
@@ -1,7 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate,login,logout
+from django.contrib import messages
 
 # Create your views here.
+@csrf_exempt
 def signup(request):
+    if request.method == "POST":
+        get_email = request.POST.get('email')
+        get_password = request.POST.get('pass1')
+        get_confirm_password = request.POST.get('pass2')
+
+        if get_password != get_confirm_password: # checking if password the user inserted are the same
+            messages.info(request,'Password is not matching...')
+            return redirect('/auth/signup/')
+
+        try: # Checking if the email already exists
+            if User.objects.get(username=get_email):
+                messages.warning(request, "Email is taken")
+                return redirect('/auth/signup/')
+        except Exception as identifier:
+            pass
+
+        myuser = User.objects.create_user(get_email,get_email,get_password)
+        myuser.save()
+        messages.success(request,'User is created. Please login.')
+        return redirect('/auth/login/')
+
     return render(request,'signup.html')
 
 def handleLogin(request):
